146bd05a6c2d4991357cc975d2b2870f1844bb87
KazuyoshiHidaka <kazuyoshi.first@gmail.com>
2020-01-01 07:01:16 +0000

    add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./App.css";
+import SearchAppBar from "./component/searchAppBar";
+import { ThemeProvider, Container } from "@material-ui/core";
+import { theme } from "./theme";
+import AnimesIndex from "./component/index_page/animesIndex";
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SearchAppBar />
+      <Container>
+        <AnimesIndex />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
